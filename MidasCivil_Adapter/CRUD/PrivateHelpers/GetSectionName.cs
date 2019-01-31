3d1ec44ad73fe61197fa4cf86cb59337eafef1e1
pnugent <Peter.Nugent@burohappold.com>
2019-01-31 16:35:10 +0000

    Closes #32

---
+++ MidasCivil_Adapter/CRUD/PrivateHelpers/GetSectionName.cs
@@ -0,0 +1,18 @@
+﻿namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+        public string GetSectionName(string text)
+        {
+
+            if(text.Contains(" "))
+            {
+                return text.Split(' ')[0].Split('*')[1];
+            }
+            else
+            {
+                return text.Split('*')[1];
+            }
+        }
+    }
+}
---
+++ MidasCivil_Adapter/CRUD/PrivateHelpers/WriteSectionText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+        private void WriteSectionText(List<string> sectionText, string section)
+        {
+            string path = directory + "\\" + section + ".txt";
+
+            using (StreamWriter sectionFile = File.CreateText(path))
+            {
+                foreach (string text in sectionText)
+                {
+                    sectionFile.WriteLine(text);
+                }
+                sectionFile.Close();
+            }
+        }
+        private void WriteSectionText(List<string> sectionText, string section, string subDirectory)
+        {
+            string path = subDirectory + "\\" + section + ".txt";
+
+            using (StreamWriter sectionFile = File.CreateText(path))
+            {
+                foreach (string text in sectionText)
+                {
+                    sectionFile.WriteLine(text);
+                }
+                sectionFile.Close();
+            }
+        }
+    }
+}
