f990e538e9c3fb872e74da85fd622cbef14c9ee9
pnugent <Peter.Nugent@burohappold.com>
2019-01-31 16:55:53 +0000

    Closes #33

--- MidasCivil_Adapter/CRUD/PrivateHelpers/GetSectionText.cs
+++ MidasCivil_Adapter/CRUD/PrivateHelpers/GetSectionText.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace BH.Adapter.MidasCivil
@@ -7,21 +8,22 @@ namespace BH.Adapter.MidasCivil
     {
         public List<string> GetSectionText(string section)
         {
-            List<int> sectionIndexes = midasText.Select((value, index) => new { value, index })
-                .Where(x => x.ToString().Contains("*"))
-                .Select(x => x.index)
-                .ToList();
+            //List<int> sectionIndexes = midasText.Select((value, index) => new { value, index })
+            //    .Where(x => x.ToString().Contains("*"))
+            //    .Select(x => x.index)
+            //    .ToList();
 
-            int nodeStart = -1;
-            List<string> sectionText = new List<string>();
+            //int nodeStart = -1;
 
-            if (midasText.Any(section.Contains))
-            {
-                nodeStart = midasText.IndexOf(
-                    midasText.FirstOrDefault(x => x.Contains(section)));
-                int nodeEnd = sectionIndexes[sectionIndexes.IndexOf(nodeStart) + 1];
-                sectionText = midasText.GetRange(nodeStart, nodeEnd - nodeStart);
-            }
+            List<string> sectionText = File.ReadAllLines(directory + "\\" + section + ".txt").ToList();
+
+            //if (midasText.Any(section.Contains))
+            //{
+            //    nodeStart = midasText.IndexOf(
+            //        midasText.FirstOrDefault(x => x.Contains(section)));
+            //    int nodeEnd = sectionIndexes[sectionIndexes.IndexOf(nodeStart) + 1];
+            //    sectionText = midasText.GetRange(nodeStart, nodeEnd - nodeStart);
+            //}
 
             CleanString(ref sectionText);
             
