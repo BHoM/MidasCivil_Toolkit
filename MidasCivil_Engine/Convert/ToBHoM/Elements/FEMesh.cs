8e06d52be96f4a2d01e2e1a29ce8299ebf90861c
pnugent <Peter.Nugent@burohappold.com>
2019-02-05 17:20:02 +0000

    Writing methods using FEMeshFace and FEMesh discussed in #5

--- MidasCivil_Engine/Convert/ToBHoM/Elements/FEMesh.cs
+++ MidasCivil_Engine/Convert/ToBHoM/Elements/FEMesh.cs
@@ -1,6 +1,4 @@
 ﻿using BH.oM.Structure.Elements;
-using BH.Engine.Geometry;
-using BH.oM.Geometry;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,38 +6,55 @@ namespace BH.Engine.MidasCivil
 {
     public static partial class Convert
     {
-        public static FEMesh ToBHoMFEMesh(this string feMesh, Dictionary<string,Node> bhomNodes)
+        public static FEMesh ToBHoMFEMesh(this string feMesh, Dictionary<string, Node> bhomNodes)
         {
             List<string> delimitted = feMesh.Split(',').ToList();
             Node n1 = null;
             Node n2 = null;
             Node n3 = null;
             Node n4 = null;
-            FEMesh bhomFEMesh;
+
 
             bhomNodes.TryGetValue(delimitted[4].Replace(" ", ""), out n1);
             bhomNodes.TryGetValue(delimitted[5].Replace(" ", ""), out n2);
             bhomNodes.TryGetValue(delimitted[6].Replace(" ", ""), out n3);
 
-            Point p1 = Engine.Structure.Query.Position(n1);
-            Point p2 = Engine.Structure.Query.Position(n2);
-            Point p3 = Engine.Structure.Query.Position(n3);
+            List<Node> nodeList = new List<Node>()
+            {
+                n1, n2, n3
+            };
 
-            if (System.Convert.ToInt32(delimitted[7].Replace(" ", "")) !=0)
+            FEMesh bhomFEMesh = null;
+
+            if (System.Convert.ToInt32(delimitted[7].Replace(" ", "")) != 0)
             {
                 bhomNodes.TryGetValue(delimitted[7].Replace(" ", ""), out n4);
-                Point p4 = Engine.Structure.Query.Position(n4);
-                List<Point> meshPoints = new List<Point> { p1, p2, p3, p4 };
-                List<Face> meshFace = new List<Face> { Geometry.Create.Face(1, 2, 3, 4) };
-                Mesh bhomMesh = Geometry.Create.Mesh(meshPoints,meshFace);
-                bhomFEMesh = Structure.Create.FEMesh(bhomMesh);
+                nodeList.Add(n4);
+                List<int> nodeListIndicies = Enumerable.Range(0, 4).ToList();
+                List<FEMeshFace> feMeshFace = new List<FEMeshFace>()
+                {
+                    new FEMeshFace() {NodeListIndices = nodeListIndicies}
+                };
+
+                bhomFEMesh = new FEMesh()
+                {
+                    MeshFaces = feMeshFace,
+                    Nodes = nodeList
+                };
             }
             else
             {
-                List<Point> meshPoints = new List<Point> { p1, p2, p3 };
-                List<Face> meshFace = new List<Face> { Geometry.Create.Face(1, 2, 3) };
-                Mesh bhomMesh = Geometry.Create.Mesh(meshPoints, meshFace);
-                bhomFEMesh = Structure.Create.FEMesh(bhomMesh);
+                List<int> nodeListIndicies = Enumerable.Range(0, 4).ToList();
+                List<FEMeshFace> feMeshFace = new List<FEMeshFace>()
+                {
+                    new FEMeshFace() {NodeListIndices = nodeListIndicies}
+                };
+
+                bhomFEMesh = new FEMesh()
+                {
+                    MeshFaces = feMeshFace,
+                    Nodes = nodeList
+                };
             }
 
             bhomFEMesh.CustomData[AdapterId] = delimitted[0].Replace(" ", "");
