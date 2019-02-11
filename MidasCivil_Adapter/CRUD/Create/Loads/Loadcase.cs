1421e1a2d7b81834222d72e172c2155b6ebb3b48
Chris Self <Chris.Self@BuroHappold.com>
2019-02-11 16:04:47 +0000

    Fixes #6, fixes #38
    Read/Write Loadcases. Midas text tile only assigns names to loadcases and not numbers hence CustomData gets assigned in writing through the name. Does not work with there are existing loadcases in the file.

---
+++ MidasCivil_Adapter/CRUD/Create/Loads/Loadcase.cs
@@ -0,0 +1,27 @@
+﻿using BH.oM.Structure.Loads;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+        private bool CreateCollection(IEnumerable<Loadcase> loadcases)
+        {
+            string path = CreateSectionFile("STLDCASE");
+            List<string> midasLoadCases = new List<string>();
+            int count = 1;
+
+            foreach (Loadcase loadcase in loadcases)
+            {
+                midasLoadCases.Add(Engine.MidasCivil.Convert.ToMCLoadCase(loadcase,count));
+                count++;
+            }
+
+            File.AppendAllLines(path, midasLoadCases);
+
+            return true;
+        }
+    }
+}
--- MidasCivil_Adapter/CRUD/Read/_Read.cs
+++ MidasCivil_Adapter/CRUD/Read/_Read.cs
@@ -7,6 +7,7 @@ using BH.oM.Structure.Elements;
 using BH.oM.Structure.Properties.Section;
 using BH.oM.Structure.Properties.Constraint;
 using BH.oM.Common.Materials;
+using BH.oM.Structure.Loads;
 
 namespace BH.Adapter.MidasCivil
 {
@@ -33,6 +34,8 @@ namespace BH.Adapter.MidasCivil
                 return ReadMaterials(ids as dynamic);
             else if (type == typeof(Constraint6DOF))
                 return Read6DOFConstraints(ids as dynamic);
+            else if (type == typeof(Loadcase))
+                return ReadLoadcases(ids as dynamic);
 
             return null;
         }
