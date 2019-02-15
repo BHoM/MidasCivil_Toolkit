e09c42501bcefb4d86d3d8b104014eb6a92becd5
Chris Self <Chris.Self@BuroHappold.com>
2019-02-15 16:43:57 +0000

    Create choose Load method and start Read PointForce

--- MidasCivil_Adapter/CRUD/Read/Loads/PointForces.cs
+++ MidasCivil_Adapter/CRUD/Read/Loads/PointForces.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using BH.oM.Structure.Loads;
+using BH.oM.Structure.Elements;
+using System.IO;
 
 namespace BH.Adapter.MidasCivil
 {
     public partial class MidasCivilAdapter
     {
-        private List<PointForce> ReadBarPointForces(List<string> ids = null)
+        private List<PointForce> ReadPointForces(List<string> ids = null)
         {
-            throw new NotImplementedException();
+            List<ILoad> bhomPointForces = new List<ILoad>();
+
+            string[] loadcases = Directory.GetDirectories(directory);
+            foreach (string loadcase in loadcases)
+            {
+                List<string> pointForceText = GetSectionText("CONLOAD",loadcase);
+                
+
+                if (pointForceText.Count!=0)
+                {
+                    List<Node> bhomNodes = ReadNodes();
+                    List<int> nodeAssignments = new List<int>();
+                    Dictionary<string, Node> nodeDictionary = bhomNodes.ToDictionary(
+                        x => x.CustomData[AdapterId].ToString());
+
+                    pointForceText.ForEach(x => nodeAssignments.Add(int.Parse(x.Split(',')[0])));
+
+                    PointForce bhomPointForce = Engine.MidasCivil.Convert.ToPointForce(
+                            lusasConcentratedLoad, nodeAssignments, nodeDictionary
+                            );
+                }
+            }
+
+            
+
+            return bhomPointForces;
         }
     }
 }
