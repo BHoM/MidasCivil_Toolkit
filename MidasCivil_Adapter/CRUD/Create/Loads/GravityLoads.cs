e6f9b616e3094e235b8a25643106a1114e6eed8c
Chris Self <Chris.Self@BuroHappold.com>
2019-03-01 16:05:55 +0000

    Closes #12

--- MidasCivil_Adapter/CRUD/Create/Loads/GravityLoads.cs
+++ MidasCivil_Adapter/CRUD/Create/Loads/GravityLoads.cs
@@ -1,6 +1,7 @@
 ﻿using BH.oM.Structure.Loads;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace BH.Adapter.MidasCivil
 {
@@ -8,7 +9,18 @@ namespace BH.Adapter.MidasCivil
     {
         public bool CreateCollection(IEnumerable<GravityLoad> gravityLoads)
         {
+            foreach (GravityLoad gravityLoad in gravityLoads)
+            {
+                List<string> midasGravityLoads = new List<string>();
+                string pointForcePath = CreateSectionFile(gravityLoad.Loadcase.Name + "\\SELFWEIGHT");
+
+                midasGravityLoads.Add(Engine.MidasCivil.Convert.ToMCGravityLoad(gravityLoad));
+
+                File.AppendAllLines(pointForcePath, midasGravityLoads);
+            }
+
             return true;
         }
+
     }
 }
