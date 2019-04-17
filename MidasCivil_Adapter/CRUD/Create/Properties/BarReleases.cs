2caeccd5d0cee5d55d33fc404d730a00ccfdf906
Chris Self <Chris.Self@BuroHappold.com>
2019-04-17 17:46:07 +0100

    Closes #93, Closes #94

---
+++ MidasCivil_Adapter/CRUD/Create/Properties/BarReleases.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Properties.Constraint;
+
+namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+        private bool CreateCollection(IEnumerable<BarRelease> releases)
+        {
+            string path = CreateSectionFile("FRAME-RLS");
+            string boundaryGroupPath = CreateSectionFile("BNDR-GROUP");
+            List<string> midasBarReleases = new List<string>();
+
+            foreach (BarRelease release in releases)
+            {
+                string midasBoundaryGroup = Engine.MidasCivil.Convert.ToMCBoundaryGroup(release.Name);
+                CompareGroup(midasBoundaryGroup, boundaryGroupPath);
+                midasBarReleases.AddRange(Engine.MidasCivil.Convert.ToMCBarRelease(release));
+            }
+
+            File.AppendAllLines(path, midasBarReleases);
+
+            return true;
+        }
+    }
+}
