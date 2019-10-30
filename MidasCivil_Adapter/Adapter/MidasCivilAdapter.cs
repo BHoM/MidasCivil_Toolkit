d52b3b8484329b5fea072be5d53a9d4a088ea5fe
peterjamesnugent <Peter.Nugent@burohappold.com>
2019-10-30 15:56:38 +0000

    Closes #155  Create() method updated and Adapter Config settings modified to align with refactoring

--- MidasCivil_Adapter/Adapter/MidasCivilAdapter.cs
+++ MidasCivil_Adapter/Adapter/MidasCivilAdapter.cs
@@ -20,12 +20,6 @@ namespace BH.Adapter.MidasCivil
             {
                 AdapterId = "MidasCivil_id";   //Set the "AdapterId" to "SoftwareName_id". Generally stored as a constant string in the convert class in the SoftwareName_Engine
 
-                Config.SeparateProperties = true;   //Set to true to push dependant properties of objects before the main objects are being pushed. Example: push nodes before pushing bars
-                Config.MergeWithComparer = true;    //Set to true to use EqualityComparers to merge objects. Example: merge nodes in the same location
-                Config.ProcessInMemory = false;     //Set to false to to update objects in the toolkit during the push
-                Config.CloneBeforePush = true;      //Set to true to clone the objects before they are being pushed through the software. Required if any modifications at all, as adding a software ID is done to the objects
-                Config.UseAdapterId = true;         //Tag objects with a software specific id in the CustomData. Requires the NextIndex method to be overridden and implemented
-
                 if (string.IsNullOrWhiteSpace(filePath))
                 {
                     throw new ArgumentException("No file path given");
