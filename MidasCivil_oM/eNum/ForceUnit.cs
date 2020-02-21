a64e251b6f9d7729cf49e5b3ef0a68ab3966660c
peterjamesnugent <Peter.Nugent@burohappold.com>
2020-02-21 15:31:33 +0000

    Fixes compliance issue for HasSingleClass

--- MidasCivil_oM/eNum/ForceUnit.cs
+++ MidasCivil_oM/eNum/ForceUnit.cs
@@ -23,23 +23,15 @@
 
 namespace BH.oM.Adapters.MidasCivil
 {
-    public enum LenghtUnit
-    {
-        M, CM, MM, FT, IN
-    }
+    /***************************************************/
+    /**** Public Enums                              ****/
+    /***************************************************/
 
     public enum ForceUnit
     {
         N, KN, KGF, TONF, LBF, KIPS
     }
 
-    public enum HeatUnit
-    {
-        CAL, KCAL, J, KJ, BTU
-    }
+    /***************************************************/
 
-    public enum TemperatureUnit
-    {
-        C, F
-    }
 }
