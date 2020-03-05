19a1691bdcf16a1593f4cb0a895296ea2f923125
peterjamesnugent <Peter.Nugent@burohappold.com>
2020-03-05 15:57:34 +0000

    Add placeholder methods for NodeResults

---
+++ MidasCivil_Adapter/CRUD/Read/Results/NodeResults.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Common;
+using BH.oM.Adapter;
+using BH.oM.Structure.Requests;
+using BH.oM.Structure.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+
+        /***************************************************/
+        /**** Public method - Read override             ****/
+        /***************************************************/
+
+        public IEnumerable<IResult> ReadResults(NodeResultRequest request, ActionConfig actionConfig)
+        {
+            List<IResult> results;
+            List<int> objectIds = GetObjectIDs(request);
+            List<int> loadCases = GetLoadcaseIDs(request);
+
+
+            switch (request.ResultType)
+            {
+                case NodeResultType.NodeReaction:
+                    results = ExtractNodeReaction(objectIds, loadCases).ToList();
+                    break;
+                case NodeResultType.NodeDisplacement:
+                    results = ExtractNodeDisplacement(objectIds, loadCases).ToList();
+                    break;
+                default:
+                    Engine.Reflection.Compute.RecordError($"Result of type {request.ResultType} is not yet supported in the MidasCivil_Toolkit.");
+                    results = new List<IResult>();
+                    break;
+            }
+            results.Sort();
+            return results;
+        }
+
+        /***************************************************/
+        /**** Private  Methods                          ****/
+        /***************************************************/
+
+        private IEnumerable<IResult> ExtractNodeReaction(List<int> ids, List<int> loadcaseIds)
+        {
+            return null;
+        }
+
+        private IEnumerable<IResult> ExtractNodeDisplacement(List<int> ids, List<int> loadcaseIds)
+        {  
+            return null;
+        }
+
+        /***************************************************/
+
+    }
+}
