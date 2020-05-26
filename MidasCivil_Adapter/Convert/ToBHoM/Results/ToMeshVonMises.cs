4108fecaaf647e8e8e6e66695a47a97653e485e8
StephennipBH <Stephen.nip@Burohappold.com>
2020-05-26 10:11:34 +0100

    Added read result MeshVonMises
    This reverts commit dc63254d3204b97ab95fe41fb7dda52702ab6740.

---
+++ MidasCivil_Adapter/Convert/ToBHoM/Results/ToMeshVonMises.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Results;
+using System.Linq;
+using System.Collections.Generic;
+using BH.oM.Geometry;
+
+namespace BH.Adapter.MidasCivil
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static MeshVonMises ToMeshVonMises(this List<string> delimitted)
+        {
+            double LayerPosition = 1;
+
+            if (delimitted[8].Contains("Bot"))
+            {
+                LayerPosition = 0;
+            }
+            MeshVonMises MeshVonMises = new MeshVonMises(System.Convert.ToInt32(delimitted[2]), delimitted[7], 0,
+            delimitted[3], 0, MeshResultLayer.Middle, LayerPosition, MeshResultSmoothingType.None, null,0,0, System.Convert.ToDouble(delimitted[15])
+            );
+
+            return MeshVonMises;
+        }
+
+        /***************************************************/
+
+    }
+}
