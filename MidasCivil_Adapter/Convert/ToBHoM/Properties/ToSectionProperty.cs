72c972a5e6fb3abaa96b4db950f377a45c77d551
Peter Nugent <Peter.Nugent@burohappold.com>
2020-06-11 16:37:13 +0100

    Fix bug for reading FabricatedISectionProfile where incorrect inputs were used

--- MidasCivil_Adapter/Convert/ToBHoM/Properties/ToSectionProperty.cs
+++ MidasCivil_Adapter/Convert/ToBHoM/Properties/ToSectionProperty.cs
@@ -74,10 +74,10 @@ namespace BH.Adapter.Adapters.MidasCivil
             }
             else if (shape == "H")
             {
-                bhomSection = Create.GenericSectionFromProfile(Create.FabricatedISectionProfile
-                    (System.Convert.ToDouble(split[14]).LengthToSI(lengthUnit), System.Convert.ToDouble(split[16]).LengthToSI(lengthUnit),
-                    System.Convert.ToDouble(split[15]).LengthToSI(lengthUnit), System.Convert.ToDouble(split[17]).LengthToSI(lengthUnit),
-                    System.Convert.ToDouble(split[18]).LengthToSI(lengthUnit), System.Convert.ToDouble(split[19]).LengthToSI(lengthUnit), 0), null);
+                bhomSection = Create.GenericSectionFromProfile(Engine.Geometry.Create.FabricatedISectionProfile
+                    (System.Convert.ToDouble(split[14]).LengthToSI(lengthUnit), System.Convert.ToDouble(split[15]).LengthToSI(lengthUnit),
+                    System.Convert.ToDouble(split[18]).LengthToSI(lengthUnit), System.Convert.ToDouble(split[16]).LengthToSI(lengthUnit),
+                    System.Convert.ToDouble(split[17]).LengthToSI(lengthUnit), System.Convert.ToDouble(split[19]).LengthToSI(lengthUnit), 0), null);
 
                 //    8, DBUSER    , USER-ISECTION     , CC, 0, 0, 0, 0, 0, 0, YES, NO, H  , 2, 1, 0.3, 0.03, 0.025, 0.5, 0.02, 0.01, 0.01, 0, 0
             }
