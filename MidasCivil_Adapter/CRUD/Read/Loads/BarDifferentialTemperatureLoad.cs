1132128bc182ef2bc7c27da26de98de60ef68b12
StephennipBH <Stephen.nip@Burohappold.com>
2021-02-16 17:47:12 +0000

    Create BarDifferentialTemperatureLoad.cs

---
+++ MidasCivil_Adapter/CRUD/Read/Loads/BarDifferentialTemperatureLoad.cs
@@ -0,0 +1,109 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using BH.oM.Adapters.MidasCivil;
+using BH.Engine.Adapter;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Elements;
+
+namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private List<ILoad> ReadBarDifferentialTemperatureLoads(List<string> ids = null)
+        {
+            List<ILoad> bhomBarDifferentialTemperatureLoads = new List<ILoad>();
+
+            List<Loadcase> bhomLoadcases = ReadLoadcases();
+            Dictionary<string, Loadcase> loadcaseDictionary = bhomLoadcases.ToDictionary(
+                        x => x.Name);
+
+            string[] loadcaseFolders = Directory.GetDirectories(m_directory + "\\TextFiles");
+
+            int i = 1;
+
+            foreach (string loadcaseFolder in loadcaseFolders)
+            {
+                string loadcase = Path.GetFileName(loadcaseFolder);
+                List<string> barDifferentialTemperatureLoadText = GetSectionText(loadcase + "\\ELTEMPER");
+
+                if (barDifferentialTemperatureLoadText.Count != 0)
+                {
+                    List<string> barComparison = new List<string>();
+                    List<string> loadedBars = new List<string>();
+
+                    foreach (string barDifferentialTemperatureLoad in barDifferentialTemperatureLoadText)
+                    {
+                        List<string> delimitted = barDifferentialTemperatureLoad.Split(',').ToList();
+                        loadedBars.Add(delimitted[0].Trim());
+                        delimitted.RemoveAt(0);
+                        barComparison.Add(String.Join(",", delimitted));
+                    }
+
+                    if (barComparison.Count != 0)
+                    {
+                        List<Bar> bhomBars = ReadBars();
+                        Dictionary<string, Bar> barDictionary = bhomBars.ToDictionary(
+                                                                    x => x.AdapterId<string>(typeof(MidasCivilId)));
+                        List<string> distinctBarLoads = barComparison.Distinct().ToList();
+
+                        foreach (string distinctBarLoad in distinctBarLoads)
+                        {
+                            List<int> indexMatches = barComparison.Select((meshload, index) => new { meshload, index })
+                                                       .Where(x => string.Equals(x.meshload, distinctBarLoad))
+                                                       .Select(x => x.index)
+                                                       .ToList();
+                            List<string> matchingBars = new List<string>();
+                            indexMatches.ForEach(x => matchingBars.Add(loadedBars[x]));
+
+                            BarDifferentialTemperatureLoad bhomBarDifferentialTemperatureLoad =
+                                Adapters.MidasCivil.Convert.ToBarDifferentialTemperatureLoad(
+                                    distinctBarLoad, matchingBars, loadcase, loadcaseDictionary, barDictionary, i, m_temperatureUnit);
+
+                            if (bhomBarDifferentialTemperatureLoad != null)
+                                bhomBarDifferentialTemperatureLoads.Add(bhomBarDifferentialTemperatureLoad);
+
+                            if ((string.IsNullOrWhiteSpace(distinctBarLoad.Split(',')[1].ToString())))
+                            {
+                                i = i + 1;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            return bhomBarDifferentialTemperatureLoads;
+        }
+
+        /***************************************************/
+
+    }
+}
