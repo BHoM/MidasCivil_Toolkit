ce5e864187404762fd997a92413f653225f2fd60
StephennipBH <Stephen.nip@Burohappold.com>
2021-03-02 16:09:42 +0000

    create read function

---
+++ MidasCivil_Adapter/CRUD/Read/Loads/AreaDifferentialTemperatureLoad.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using BH.oM.Adapters.MidasCivil;
+using BH.Engine.Adapter;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Elements;
+
+namespace BH.Adapter.MidasCivil
+{
+    public partial class MidasCivilAdapter
+    {
+        private List<ILoad> ReadAreaDifferentialTemperatureLoads(List<string> ids = null)
+        {
+            List<ILoad> bhomAreaDifferentialTemperatureLoads = new List<ILoad>();
+
+            List<Loadcase> bhomLoadcases = ReadLoadcases();
+            Dictionary<string, Loadcase> loadcaseDictionary = bhomLoadcases.ToDictionary(
+                        x => x.Name);
+
+            string[] loadcaseFolders = Directory.GetDirectories(m_directory + "\\TextFiles");
+
+            int i = 1;
+
+            foreach (string loadcaseFolder in loadcaseFolders)
+            {
+                string loadcase = Path.GetFileName(loadcaseFolder);
+                List<string> areaDifferentialTemperatureLoadText = GetSectionText(loadcase + "\\THERGRAD");
+
+                if (areaDifferentialTemperatureLoadText.Count != 0)
+                {
+                    List<string> feMeshComparison = new List<string>();
+                    List<string> loadedFEMeshes = new List<string>();
+
+                    foreach (string areaDifferentialTemperatureLoad in areaDifferentialTemperatureLoadText)
+                    {
+                        List<string> delimitted = areaDifferentialTemperatureLoad.Split(',').ToList();
+                        loadedFEMeshes.Add(delimitted[0].Trim());
+                        delimitted.RemoveAt(0);
+                        feMeshComparison.Add(String.Join(",", delimitted));
+                    }
+
+                    if (feMeshComparison.Count != 0)
+                    {
+                        List<FEMesh> bhomMeshes = ReadFEMeshes();
+                        Dictionary<string, FEMesh> FEMeshDictionary = bhomMeshes.ToDictionary(
+                                                                    x => x.AdapterId<string>(typeof(MidasCivilId)));
+                        List<string> distinctFEMeshLoads = feMeshComparison.Distinct().ToList();
+
+                        foreach (string distinctFEMeshLoad in distinctFEMeshLoads)
+                        {
+                            List<int> indexMatches = feMeshComparison.Select((meshload, index) => new { meshload, index })
+                                                       .Where(x => string.Equals(x.meshload, distinctFEMeshLoad))
+                                                       .Select(x => x.index)
+                                                       .ToList();
+                            List<string> matchingFEMeshes = new List<string>();
+                            indexMatches.ForEach(x => matchingFEMeshes.Add(loadedFEMeshes[x]));
+
+                            AreaDifferentialTemperatureLoad bhomAreaDifferentialTemperatureLoad =
+                                Adapters.MidasCivil.Convert.ToAreaDifferentialTemperatureLoad(
+                                    distinctFEMeshLoad, matchingFEMeshes, loadcase, loadcaseDictionary, FEMeshDictionary, i, m_temperatureUnit);
+
+                            if (bhomAreaDifferentialTemperatureLoad != null)
+                                bhomAreaDifferentialTemperatureLoads.Add(bhomAreaDifferentialTemperatureLoad);
+
+                            if ((string.IsNullOrWhiteSpace(distinctFEMeshLoad.Split(',')[1].ToString())))
+                            {
+                                i = i + 1;
+                            }
+
+                        }
+
+                    }
+                }
+            }
+            return bhomAreaDifferentialTemperatureLoads;
+        }
+
+    }
+}
---
+++ MidasCivil_Adapter/Convert/ToBHoM/Loads/ToAreaDifferentialTemperatureLoad.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using BH.oM.Adapters.MidasCivil;
+using BH.Engine.Adapter;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.Elements;
+
+namespace BH.Adapter.Adapters.MidasCivil
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static AreaDifferentialTemperatureLoad ToAreaDifferentialTemperatureLoad(string temperatureLoad, List<string> associatedFEMeshes, string loadcase,
+            Dictionary<string, Loadcase> loadcaseDictionary, Dictionary<string, FEMesh> femeshDictionary, int count, string temperatureUnit)
+        {
+            string[] delimitted = temperatureLoad.Split(',');
+            List<FEMesh> bhomAssociatedFEMeshes = new List<FEMesh>();
+
+            Loadcase bhomLoadcase;
+            loadcaseDictionary.TryGetValue(loadcase, out bhomLoadcase);
+
+            foreach (string associatedFEMesh in associatedFEMeshes)
+            {
+                if (femeshDictionary.ContainsKey(associatedFEMesh))
+                {
+                    FEMesh bhomAssociatedFEMesh;
+                    femeshDictionary.TryGetValue(associatedFEMesh, out bhomAssociatedFEMesh);
+                    bhomAssociatedFEMeshes.Add(bhomAssociatedFEMesh);
+                }
+            }
+
+            double temperature = double.Parse(delimitted[1].Trim());
+            double topTemperature = temperature / 2;
+            double botTemperature = -topTemperature;
+
+            string name;
+
+            if (string.IsNullOrWhiteSpace(delimitted[4]))
+            {
+                name = "ADTL" + count;
+            }
+            else
+            {
+                name = delimitted[4].Trim();
+            }
+
+            if (bhomAssociatedFEMeshes.Count != 0)
+            {
+                AreaDifferentialTemperatureLoad bhomAreaDifferentialTemperatureLoad = Engine.Structure.Create.AreaDifferentialTemperatureLoad(
+                    bhomLoadcase, topTemperature.DeltaTemperatureToSI(temperatureUnit), botTemperature.DeltaTemperatureToSI(temperatureUnit),
+                    bhomAssociatedFEMeshes, name);
+                bhomAreaDifferentialTemperatureLoad.SetAdapterId(typeof(MidasCivilId), bhomAreaDifferentialTemperatureLoad.Name);
+                return bhomAreaDifferentialTemperatureLoad;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /***************************************************/
+
+    }
+}
+
+
