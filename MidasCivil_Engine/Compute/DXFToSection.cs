573cb2c696df29bdd69bd2ac71ad6e00edbb7cae
calumlockhart-bh <calum.lockhart@burohappold.com>
2021-05-10 17:47:11 +0100

    DXF
    uses existing component. (uses LGPL license so should be fine to use in the BHoM)

---
+++ MidasCivil_Engine/Compute/DXFToSection.cs
@@ -0,0 +1,169 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using BH.oM.Structure.SectionProperties;
+using netDxf;
+using BH.oM.Geometry;
+using BH.oM.Spatial.ShapeProfiles;
+using BH.oM.Structure.MaterialFragments;
+using System;
+
+namespace BH.Engine.Adapters.MidasCivil
+{
+    public static partial class Create
+    {
+        public static GenericSection DXFToGenericSection(string filePath, string name, bool activate)
+        {
+            if (activate)
+            {
+                GenericSection section = Structure.Create.GenericSectionFromProfile(DXFToFreeFormProfile(filePath, name, true), DXFMaterialProperties(filePath), name);
+                Engine.Reflection.Compute.ClearCurrentEvents();
+                Engine.Reflection.Compute.RecordWarning(".dxf file units are assumed to be in milimeters and will be converted to meters");
+                Engine.Reflection.Compute.RecordWarning("Can not calculate Torsional or Warping constants from DXF file, these values are assumed to be zero");
+                return section;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static FreeFormProfile DXFToFreeFormProfile(string filePath, string name, bool activate)
+        {
+            if (activate)
+            {
+                DxfDocument DXFdoc = netDxf.DxfDocument.Load(filePath);
+                List<ICurve> edges = new List<ICurve>();
+
+                foreach (netDxf.Entities.Line line in DXFdoc.Lines)
+                {
+                    edges.Add(BHOMLine(line));
+                }
+                foreach (netDxf.Entities.Polyline line in DXFdoc.Polylines)
+                {
+                    edges.Add(BHOMPolyline(line));
+                }
+                foreach (netDxf.Entities.Circle circle in DXFdoc.Circles)
+                {
+                    edges.Add(BHOMCircle(circle));
+                }
+
+                FreeFormProfile profile = new FreeFormProfile(edges);
+                return profile;
+            }
+            else { return null; }
+        }
+        private static Point BHOMPoint(netDxf.Vector3 point)
+        {
+            Point point_ = new Point();
+            point_.X = point.X / 1000;
+            point_.Y = point.Y / 1000;
+            point_.Z = point.Z / 1000;
+            return point_;
+        }
+        private static Line BHOMLine(netDxf.Entities.Line line)
+        {
+            Line line_ = new Line();
+            line_.Start = BHOMPoint(line.StartPoint);
+            line_.End = BHOMPoint(line.EndPoint);
+            return line_;
+        }
+
+        private static Polyline BHOMPolyline(netDxf.Entities.Polyline line)
+        {
+            List<Point> points = new List<Point>();
+            foreach (Vector3 point in (IEnumerable<Vector3>)line)
+            {
+                points.Add(BHOMPoint(point));
+            }
+            Polyline line_ = new Polyline();
+            line_.ControlPoints = points;
+            return line_;
+        }
+
+        private static Circle BHOMCircle(netDxf.Entities.Circle circle)
+        {
+            Point centre = BHOMPoint(circle.Center);
+            double radius = circle.Radius / 1000;
+
+            Circle circle_ = new Circle();
+            circle_.Radius = radius;
+            circle_.Centre = centre;
+            return circle_;
+        }
+
+        private static IMaterialFragment DXFMaterialProperties(string filePath)
+        {
+            DxfDocument DXFDoc = DxfDocument.Load(filePath);
+            foreach (netDxf.Entities.MText text in DXFDoc.MTexts)
+            {
+                string text_ = text.Value.Replace(" ", "");
+                if (text_.Contains("MATERIALPROPERTIES"))
+                {
+                    if (text_.Contains("PoissonsRatioX") || text_.Contains("PoissonsRatioY") || text_.Contains("PoissonsRatioZ") ||
+                        text_.Contains("ThermalExpansionCoefficientX") || text_.Contains("ThermalExpansionCoefficientY") || text_.Contains("ThermalExpansionCoefficientZ") ||
+                        text_.Contains("YoungsModulusX") || text_.Contains("YoungsModulusY") || text_.Contains("YoungsModulusZ"))
+                    {
+                        GenericOrthotropicMaterial material = new GenericOrthotropicMaterial();
+                        string[] properties = text_.Split(new string[] { "\\P" }, StringSplitOptions.None);
+                        foreach (string property in properties)
+                        {
+                            if (property.Contains("Name")) { material.Name = property.Split(':')[1]; }
+                            else if (property.Contains("DampingRatio")) { material.DampingRatio = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("Density")) { material.Density = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("PoissonsRatioX")) { material.PoissonsRatio.X = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("PoissonsRatioY")) { material.PoissonsRatio.Y = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("PoissonsRatioZ")) { material.PoissonsRatio.Z = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("ThermalExpansionCoefficientX")) { material.ThermalExpansionCoeff.X = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("ThermalExpansionCoefficientY")) { material.ThermalExpansionCoeff.Y = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("ThermalExpansionCoefficientY")) { material.ThermalExpansionCoeff.Z = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("YoungsModulusX")) { material.YoungsModulus.X = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("YoungsModulusY")) { material.YoungsModulus.Y = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("YoungsModulusZ")) { material.YoungsModulus.Z = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                        }
+                        return material;
+                    }
+                    else
+                    {
+                        GenericIsotropicMaterial material = new GenericIsotropicMaterial();
+                        string[] properties = text_.Split(new string[] { "\\P" }, StringSplitOptions.None);
+                        foreach (string property in properties)
+                        {
+                            if (property.Contains("Name")) { material.Name = property.Split(':')[1]; }
+                            else if (property.Contains("DampingRatio")) { material.DampingRatio = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("Density")) { material.Density = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("PoissonsRatio")) { material.PoissonsRatio = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("ThermalExpansionCoefficient")) { material.ThermalExpansionCoeff = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                            else if (property.Contains("YoungsModulus")) { material.YoungsModulus = double.Parse(property.Split(':')[1], System.Globalization.CultureInfo.InvariantCulture); }
+                        }
+                        return material;
+                    }
+                }
+            }
+            Engine.Reflection.Compute.RecordError("No material properties are associated with this dxf file");
+            return null;
+        }
+    }
+}
+
+
---
+++ MidasCivil_Engine/Compute/SectionToDXF.cs
@@ -0,0 +1,194 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using System.IO;
+using BH.oM.Structure.SectionProperties;
+using netDxf;
+using BH.oM.Geometry;
+using BH.oM.Spatial.ShapeProfiles;
+using BH.oM.Structure.MaterialFragments;
+
+namespace BH.Engine.Adapters.MidasCivil
+{
+    public static partial class Compute
+    {
+        public static bool sectionToDXF(IGeometricalSection section, string directory, string name, bool activate)
+        {
+            if (activate)
+            {
+                DxfDocument DXFdoc = new DxfDocument();
+
+                IProfile profile = section.SectionProfile;
+                IEnumerable<ICurve> edges = profile.Edges;
+
+                netDxf.AciColor colour = GetColourFromType(section);
+                netDxf.Tables.Layer layer = new netDxf.Tables.Layer(name);
+                layer.Color = colour;
+
+                foreach (BH.oM.Geometry.ICurve edge in edges)
+                {
+                    if (edge.GetType() == typeof(Line))
+                    {
+                        netDxf.Entities.Line line = DXFLine((Line)edge);
+                        line.Layer = layer;
+                        DXFdoc.AddEntity(line);
+                    }
+
+                    else if (edge.GetType() == typeof(Polyline))
+                    {
+                        netDxf.Entities.Polyline line = DXFPolyline((Polyline)edge);
+                        line.Layer = layer;
+                        DXFdoc.AddEntity(line);
+                    }
+
+                    else if (edge.GetType() == typeof(Circle))
+                    {
+                        netDxf.Entities.Circle line = DXFCircle((Circle)edge);
+                        line.Layer = layer;
+                        DXFdoc.AddEntity(line);
+                    }
+                    else if (edge.GetType() == typeof(PolyCurve))
+                    {
+                        foreach(ICurve curve in ((PolyCurve)edge).Curves)
+                        {
+                            if (edge.GetType() == typeof(Line))
+                            {
+                                netDxf.Entities.Line line = DXFLine((Line)edge);
+                                line.Layer = layer;
+                                DXFdoc.AddEntity(line);
+                            }
+
+                            else if (edge.GetType() == typeof(Polyline))
+                            {
+                                netDxf.Entities.Polyline line = DXFPolyline((Polyline)edge);
+                                line.Layer = layer;
+                                DXFdoc.AddEntity(line);
+                            }
+                        }
+                    }
+                }
+                
+                netDxf.Entities.MText MText = new netDxf.Entities.MText(PropertyTextBlock(section.Material));
+                netDxf.Tables.Layer pLayer = new netDxf.Tables.Layer(name + "Properties");
+                pLayer.IsVisible = false;
+                MText.Layer = pLayer;
+                pLayer.Color = AciColor.DarkGray;
+
+                DXFdoc.AddEntity(MText);
+
+                DXFdoc.DrawingVariables.AUnits = netDxf.Units.AngleUnitType.DecimalDegrees;
+                DXFdoc.DrawingVariables.LUnits = netDxf.Units.LinearUnitType.Decimal;
+                DXFdoc.DrawingVariables.InsUnits = netDxf.Units.DrawingUnits.Millimeters;
+                Engine.Reflection.Compute.RecordWarning(".dxf file units have been set to milimeters");
+
+                DXFdoc.Save(directory + "\\" + name + ".dxf");
+                return true;
+
+            }
+            return false;
+        }
+
+        private static netDxf.AciColor GetColourFromType(IGeometricalSection section)
+        {
+            if (section.GetType() == typeof(SteelSection))
+            {
+                return AciColor.Red;
+            }
+            else if (section.GetType() == typeof(ConcreteSection))
+            {
+                return AciColor.Green;
+            }
+            else if (section.GetType() == typeof(AluminiumSection))
+            {
+                return AciColor.Yellow;
+            }
+            else if (section.GetType() == typeof(CableSection))
+            {
+                return AciColor.Blue;
+            }
+            else
+            {
+                return AciColor.LightGray;
+            }
+        }
+
+        private static netDxf.Vector3 DXFPoint(Point point)
+        {
+            netDxf.Vector3 point_ = new netDxf.Vector3(point.X * 1000, point.Y * 1000, point.Z * 1000);
+            return point_;
+        }
+        private static netDxf.Entities.Line DXFLine(Line line)
+        {
+            netDxf.Entities.Line line_ = new netDxf.Entities.Line(DXFPoint(line.Start), DXFPoint(line.End));
+            return line_;
+        }
+        private static netDxf.Entities.Circle DXFCircle(Circle circle)
+        {
+            netDxf.Entities.Circle circle_ = new netDxf.Entities.Circle(DXFPoint(circle.Centre), circle.Radius * 1000);
+            return circle_;
+        }
+        private static netDxf.Entities.Polyline DXFPolyline(Polyline polyline)
+        {
+            List<netDxf.Vector3> points = new List<netDxf.Vector3>();
+            foreach (Point point in polyline.ControlPoints)
+            {
+                points.Add(DXFPoint(point));
+            }
+            netDxf.Entities.Polyline polyline_ = new netDxf.Entities.Polyline(points);
+            return polyline_;
+        }
+        private static string PropertyTextBlock(IMaterialFragment material)
+        {
+            string properties = "MATERIAL PROPERTIES\\P" +
+                $"Name:   {material.Name}\\P" +
+                $"Damping Ratio:   {material.DampingRatio.ToString()}\\P" +
+                $"Density:   {material.Density.ToString()}\\P";
+
+            if (material is IIsotropic)
+            {
+                IIsotropic isotropic = material as IIsotropic;
+                properties += 
+                    $"Poissons Ratio:   {isotropic.PoissonsRatio}\\P" +
+                    $"Thermal Expansion Coefficient:   {isotropic.ThermalExpansionCoeff}\\P" +
+                    $"Youngs Modulus:   {isotropic.YoungsModulus}\\P";
+            }
+            else if (material is IOrthotropic)
+            {
+                IOrthotropic orthotropic = material as IOrthotropic;
+                properties +=
+                    $"Poissons Ratio X:   {orthotropic.PoissonsRatio.X}\\P" +
+                    $"Poissons Ratio Y:   {orthotropic.PoissonsRatio.Y}\\P" +
+                    $"Poissons Ratio Z:   {orthotropic.PoissonsRatio.Z}\\P" +
+                    $"Thermal Expansion Coefficient X:   {orthotropic.ThermalExpansionCoeff.X}\\P" +
+                    $"Thermal Expansion Coefficient Y:   {orthotropic.ThermalExpansionCoeff.Y}\\P" +
+                    $"Thermal Expansion Coefficient Z:   {orthotropic.ThermalExpansionCoeff.Z}\\P" +
+                    $"Youngs Modulus X:   {orthotropic.YoungsModulus.X}\\P" +
+                    $"Youngs Modulus Y:   {orthotropic.YoungsModulus.Y}\\P" +
+                    $"Youngs Modulus Z:   {orthotropic.YoungsModulus.Z}\\P";
+            }
+
+            return properties;
+        }
+    }
+}
+
